455c9369837777208c327b1daee4bb4ff8ae9c9f
Yann <birbayann@gmail.com>
2023-02-28 10:38:48 +0100

    fix (create user): throw error instead of return null on error

--- src/utils/getToken.ts
+++ src/utils/getToken.ts
@@ -2,7 +2,8 @@ import { sign } from "jsonwebtoken";
 import { User } from "../entities/User/User";
 
 export const getToken = (user: User) => {
-  if (!process.env.JWT_SECRET) return null;
+  if (!process.env.JWT_SECRET)
+    throw new Error("JWT_SECRET env variable is not defined");
   try {
     const token = sign(
       {
@@ -11,7 +12,7 @@ export const getToken = (user: User) => {
       process.env.JWT_SECRET
     );
     return token;
-  } catch (error) {
-    return null;
+  } catch (error: any) {
+    throw new Error(error);
   }
 };
